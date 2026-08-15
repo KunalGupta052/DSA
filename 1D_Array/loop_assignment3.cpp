69e56c7623f9fe756f368509a2eac90759e72d18
Kunal Gupta <srkdkunal8967@gmail.com>
2026-08-15 19:03:40 +0530

    loop_assignment_3

---
+++ 1D_Array/loop_assignment3.cpp
@@ -0,0 +1,179 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+// #include <iostream>
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n; i++){
+//         for(int j=1; j<=n; j++){
+//             if(i==n || j==1  ){
+//                 cout<<"*";
+                
+//             }
+            
+        
+//         }
+//     }
+
+//     return 0;
+// }
+
+// 1. Simple number pattern:
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=i; j++){
+            
+//             if(j==1){
+//                 cout<<"1";
+//             }
+//             if(j==2){
+               
+//                 cout<<"2";
+//             }
+//             if(j==3){
+                
+//                 cout<<"3";
+//             }
+//             if(j==4){
+                
+//                 cout<<"4";
+//             }
+//             if(j==5){
+               
+//                 cout<<"5";
+//             }
+//         }
+        
+//         cout<<endl;
+//     }
+    
+//     return 0;
+// }
+
+//
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=i; j<=n; j++){
+            
+//             if(j==1){
+//                 cout<<"1";
+//             }
+//             if(j==2){
+               
+//                 cout<<"2";
+//             }
+//             if(j==3){
+                
+//                 cout<<"3";
+//             }
+//             if(j==4){
+                
+//                 cout<<"4";
+//             }
+//             if(j==5){
+               
+//                 cout<<"5";
+//             }
+//         }
+        
+//         cout<<endl;
+//     }
+    
+//     return 0;
+// }
+
+
+
+// 3. Square of numbers:
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=n; j++){
+            
+//             cout<<i;
+//         }
+//         cout<<endl;
+//     }
+    
+//     return 0;
+// }
+
+
+//4. Repeated number pattern:
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=i; j++){
+            
+//             cout<<i;
+//         }
+//         cout<<endl;
+//     }
+    
+//     return 0;
+// }
+
+// // 6. Inverted number triangle:
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=n-i+1; j++){
+            
+//             if(j==1){
+//                 cout<<"1";
+//             }
+//             if(j==2){
+               
+//                 cout<<"2";
+//             }
+//             if(j==3){
+                
+//                 cout<<"3";
+//             }
+//             if(j==4){
+                
+//                 cout<<"4";
+//             }
+//             if(j==5){
+               
+//                 cout<<"5";
+//             }
+//         }
+        
+//         cout<<endl;
+//     }
+    
+//     return 0;
+// }
+
+
+// 7. Floyd's triangle:
+
+
+
+
