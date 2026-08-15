77631468fc5e876f61411b923ed7c1d9044ada95
Kunal Gupta <srkdkunal8967@gmail.com>
2026-08-15 11:45:00 +0530

    loop_assignment_1

---
+++ 1D_Array/loop assignment-1.cpp
@@ -0,0 +1,162 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         cout<<i<<" ";
+//     }
+
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     int sum = 0;
+//     for(int i=1;i<=n;i++){
+//         sum+=i;
+//     }
+//     cout<<sum;
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     int sum;
+//     sum=n*(n+1)/2;
+//     cout<<sum;
+    
+
+//     return 0;
+// }
+
+//multiplication table
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int m;
+//     for(int i=1;i<=n;i++){
+//         m=n*i;
+//         cout<<n<<'x'<<i<<'='<<m<<endl;
+//     }
+    
+//     return 0;
+// }
+
+
+//Reverse printing: Print numbers from N down to 1.
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+    
+//     for(int i=n;i>=1;i--){
+//         cout<<i<<" ";
+//     }
+    
+//     return 0;
+// }
+
+
+
+//Reverse a number: Reverse the digits of a number N.
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int  digit = 0;
+//     int rev = 0;
+//     while(n>0){
+//         digit = n % 10;
+//         rev = rev*10 + digit;
+//         n = n/10;
+//     }
+//     cout<<rev;
+    
+//     return 0;
+// }
+
+
+//Power of number: Find Power b of number a.
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int a;
+//     cin>>a;
+//     int b;
+//     b = log(a);
+//     cout<<b;
+   
+    
+//     return 0;
+// }
+
+//  Count digits of a number: Count the number of digits in an integer N.
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+    
+//     int n;
+//     cin>>n;
+//     int count =0 ;
+//     int digit;
+//     while(n>0){
+//         digit = n % 10;
+//         n = n / 10;
+//         count += 1;
+//     }
+//     cout<<count;
+    
+//     return 0;
+// }
+
+
+// Sum of digits: Find the sum of all digits in a number 𝑁.
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+    
+//     int n;
+//     cin>>n;
+//     int sum =0 ;
+//     int digit;
+//     while(n>0){
+//         digit = n % 10;
+//         n = n / 10;
+//         sum+=digit;
+//     }
+//     cout<<sum;
+    
+//     return 0;
+// }
+
+
+
+
