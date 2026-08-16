b0acfe5817ed8dd84abb5d60a4ee1e2615536af9
Kunal Gupta <srkdkunal8967@gmail.com>
2026-08-16 23:54:53 +0530

    update_in_loop_ssignment_3

--- 1D_Array/loop_assignment3.cpp
+++ 1D_Array/loop_assignment3.cpp
@@ -177,3 +177,251 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 
 
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+    
+//     for(int i=1; i<=n; i++){
+//         for(int j=1;j<=n-i; j++){
+//             cout<<"  ";
+//         }
+//         for(int k=1; k<=i; k++){
+//             cout<<k<<" ";
+        
+//         }
+//         for(int l=i-1; l>=1; l--){
+//             cout<<l<<" ";
+//         }
+//         cout<<endl;
+//     }
+        
+
+//     return 0;
+// }
+
+
+
+
+// 9.Diamond shaped number:
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+    
+//     for(int i=1; i<=n; i++){
+//         for(int j=1;j<=n-i; j++){
+//             cout<<"  ";
+//         }
+//         for(int k=1; k<=i; k++){
+//             cout<<k<<" ";
+        
+//         }
+//         for(int l=i-1; l>=1; l--){
+//             cout<<l<<" ";
+//         }
+//         cout<<endl;
+//     }
+     
+//     for(int i=1; i<=n; i++){
+//         for(int j=1;j<=i; j++){
+//             cout<<"  ";
+//         }
+//         for(int k=1; k<=n-i; k++){
+//             cout<<k<<" ";
+        
+//         }
+//         for(int l=n-i-1; l>=1; l--){
+//             cout<<l<<" ";
+//         }
+//         cout<<endl;
+//     }   
+
+//     return 0;
+// }
+
+
+
+//8. Number Pyramid
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1;j<=n-i; j++){
+//             cout<<"  ";
+//         }
+//         for(int k=1; k<=i; k++){
+            
+//             cout<<k<<"   ";
+            
+            
+//         }
+        
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+//
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+    
+//     for(int i=1; i<=n; i++){
+//         for(int j=1;j<=n-i; j++){
+//             cout<<"  ";
+//         }
+//         for(int k=1; k<=i; k++){
+//             cout<<k<<" ";
+        
+//         }
+//         for(int l=i-1; l>=1; l--){
+//             cout<<l<<" ";
+//         }
+//         cout<<endl;
+//     }
+        
+
+//     return 0;
+// }
+
+
+
+
+// 9.Diamond shaped number:
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+    
+//     for(int i=1; i<=n; i++){
+//         for(int j=1;j<=n-i; j++){
+//             cout<<"  ";
+//         }
+//         for(int k=1; k<=i; k++){
+//             cout<<k<<" ";
+        
+//         }
+//         for(int l=i-1; l>=1; l--){
+//             cout<<l<<" ";
+//         }
+//         cout<<endl;
+//     }
+     
+//     for(int i=1; i<=n; i++){
+//         for(int j=1;j<=i; j++){
+//             cout<<"  ";
+//         }
+//         for(int k=1; k<=n-i; k++){
+//             cout<<k<<" ";
+        
+//         }
+//         for(int l=n-i-1; l>=1; l--){
+//             cout<<l<<" ";
+//         }
+//         cout<<endl;
+//     }   
+
+//     return 0;
+// }
+
+
+
+// 10. Number pyramid
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1;j<=n-i; j++){
+//             cout<<"  ";
+//         }
+//         for(int k=1; k<=i; k++){
+            
+//             cout<<k<<"   ";
+            
+            
+//         }
+        
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+// 11.constant pyramid
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1;j<=n-i; j++){
+//             cout<<"  ";
+//         }
+//         for(int k=1; k<=i; k++){
+            
+//             cout<<i<<"   ";
+            
+            
+//         }
+        
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+// Hollow number pyramid
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=1;j<=n-i; j++){
+            cout<<"  ";
+        }
+        for(int k=1; k<=i; k++){
+            if(i==1 || i==n){
+                cout<<k<<"   ";
+            }
+            // if(k==i ){
+            //     cout<<k<<"   ";
+            // }
+        
+            
+            
+            
+        }
+        
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
+
+
