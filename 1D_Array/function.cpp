7399e7a41b5ff8fc2e7785ce0537c7c423c75f4b
Kunal Gupta <srkdkunal8967@gmail.com>
2026-08-18 18:51:56 +0530

    functions

---
+++ 1D_Array/function.cpp
@@ -0,0 +1,150 @@
+// /******************************************************************************
+
+//                               Online C++ Compiler.
+//               Code, Compile, Run and Debug C++ program online.
+// Write your code in this editor and press "Run" button to compile and execute it.
+
+// *******************************************************************************/
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     String text, inc, dec;
+//     int key;
+//     cout<<"Enter Key";
+//     getline(cin,text);
+//     cout<<"Enter Key: ";
+//     cin>>key;
+//     inc = text;
+//     for(char &c : in c){
+//         if(isupper(c)){
+//             c = ((c-'A')+key)%26 + 'A';
+            
+//         }
+//         else if(islower(c)){
+//             c = (c-'a'+key)%26 + 'a';
+//         }
+//         cout<<"Encrypted text: "<< inc<<endl;
+//         dec = inc;
+//         for(char &c : dec){
+//             if(isupper(c))
+//         }
+//     }
+    
+
+//     return 0;
+// }
+
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// void square(*p, *q ){
+    
+//     int *p = 54;
+//     int *q = 90;
+    
+//     cout<<*p<<" "<<*q<<endl;
+// }
+// int main(){
+    
+//     int a=5;
+//     int b=7;
+    
+//     square(&a, &b);
+    
+//     cout<<<a<<" "<<b<<endl;
+    
+//     return 0;
+// }
+
+// we can change the value by derefrencing in call by refrencing
+// call by address:
+
+
+
+// vowel
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+    
+//     char c;
+//     cin>>c;
+    
+//     if(c=='a'||'e'||'i'||'o'||'u'||'A'||'E'||'I'||'O'||'U'){
+//         cout<<"Vowels";
+//     }
+//     else{
+//         cout<<"Consonants";
+//     }
+    
+    
+    
+//     return 0;
+// }
+
+
+//sign -ve
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+    
+//     if(n<0){
+//         cout<<"negative";
+//     }
+//     else if(n>0){
+//         cout<<"positive";
+//     }
+//     else{
+//         cout<<"zero";
+//     }
+    
+    
+//     return 0;
+// }
+
+
+// divisibility 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+    
+//     int n;
+//     cin>>n;
+//     if(n%3==0 && n%5==0){
+//         cout<<"divisible by 3 and 5";
+//     }
+//     else{
+//         cout<<"not divisible";
+//     }
+//     return 0;
+// }
+
+
+//check alphabetic
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+    
+//     if(n<0){
+//         cout<<"Invalid ";
+//     }
+//     else{
+        
+        
+//     }
+    
+    
+    
+//     return 0;
+// }
+
+
